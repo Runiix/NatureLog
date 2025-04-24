61d9bde10c4402a1053a0a348a5cfef9123ad622
Ruben Liebert <rubenliebert@gmail.com>
2025-04-24 22:05:22 +0200

    moved loading spinner

--- app/(user_pages)/loading.tsx
+++ app/(user_pages)/loading.tsx
@@ -2,8 +2,8 @@ import { CircleLoader } from "react-spinners";
 
 export default function Loading() {
   return (
-    <div className="text-green-600 text-5xl mt-20 w-full h-full flex items-center justify-center">
-      Seite wird geladen <CircleLoader color="#16A34A" />
+    <div className="text-green-600 text-5xl mt-64 w-full h-full flex items-center justify-center">
+      <CircleLoader color="#16A34A" />
     </div>
   );
 }
